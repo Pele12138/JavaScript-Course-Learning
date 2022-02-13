445304ea19a393455b826b9d3ee4be18b1e30f01
Pele <pqq12138@outlook.com>
2022-02-13 20:48:49 +0800

    2.13 variables & values

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,14 @@
+let js = 'JavaScript';
+if (js === 'JavaScript') {
+    // alert('JavaScript is Fun')
+}
+
+// use camelCase to name a variable _uderscore
+let firstName = "Pele";
+let lastName$ = "Pan";
+console.log(45 + 46 + 22 + 12);
+console.log(firstName);
+console.log(lastName$);
+console.log(firstName);
+console.log(firstName);
+let PI = 3.14159;
