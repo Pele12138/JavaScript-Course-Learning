56e09eda4efd4c86a65c5bd235ed3df2abefcb7f
Pele <pqq12138@outlook.com>
2022-02-15 19:13:33 +0800

    2.15 Learning

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -4,11 +4,42 @@ if (js === 'JavaScript') {
 }
 
 // use camelCase to name a variable _uderscore
-let firstName = "Pele";
+/*let firstName = "Pele";
 let lastName$ = "Pan";
 console.log(45 + 46 + 22 + 12);
 console.log(firstName);
 console.log(lastName$);
 console.log(firstName);
 console.log(firstName);
-let PI = 3.14159;
+let PI = 3.14159;*/
+/*
+const contry = "China"
+const continent = "Asia"
+let population = "1400 million"
+
+
+const isIsland = false;
+let language;
+console.log(contry)
+console.log(continent)
+console.log(population)
+console.log(isIsland)
+console.log(language)
+
+language = 'Chinese';
+*/
+
+const firstName = "Pele";
+const lastName = "Pan";
+console.log(firstName, lastName)
+console.log('Name:', firstName + ' ' + lastName);
+const bornYear = 2001;
+let age = 2022 - bornYear;
+console.log('Age: ', age)
+let isFullAge = age >= 18;
+console.log(isFullAge)
+
+let x, y = 10;
+x = y /= 2;
+// excute/calculate from right 2 left
+console.log(x, y)
