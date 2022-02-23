a8b4297a50ba9923062f29456b7515d07a1c0f8d
Pele <pqq12138@outlook.com>
2022-02-24 07:59:56 +0800

    complete challenge #3

--- Challenge #3/Challenge 3.js
+++ Challenge #3/Challenge 3.js
@@ -1,5 +1,5 @@
-let Dolphins = [96, 108, 89];
-let Koalas = [88, 91, 110];
+let Dolphins = [97, 112, 101];
+let Koalas = [109, 95, 106];
 
 function average(arr) {
     var sum = 0;
@@ -10,6 +10,8 @@ function average(arr) {
 let avrDolphins = average(Dolphins);
 let avrkoalas = average(Koalas);
 
+console.log(avrDolphins);
+console.log(avrkoalas);
 /* if ((avrDolphins > avrkoalas)) {
     console.log("Dolphins win");
 } else if (avrDolphins === avrkoalas) {
@@ -19,4 +21,12 @@ else {
     console.log("Koalas win");
 } */
 
-if ()
+if (avrDolphins > avrkoalas && avrDolphins >= 100) {
+    console.log("Dophins win! 🏆");
+} else if (avrDolphins < avrkoalas && avrkoalas > 100) {
+    console.log("Koalas win! 🏆");
+} else if (avrDolphins === avrkoalas && avrDolphins > 100 && avrkoalas > 100) {
+    console.log("Both win the 🏆")
+} else {
+    console.log("No one wins the 🏆");
+}
