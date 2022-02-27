5ef0b43a9279730e98d21b572e7530cf84201157
Pele <70554344+Pele12138@users.noreply.github.com>
2022-02-27 08:43:03 +0800

    Start Fundamental 2

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,9 @@
+// 'use strcit';
+
+function fruitProcessor(apples, oranges) {
+    console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+    return juice;
+}
+
+console.log(fruitProcessor(1, 2));
