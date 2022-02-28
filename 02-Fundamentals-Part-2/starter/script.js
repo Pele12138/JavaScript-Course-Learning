b8537237e972f997adc094ba74dd3a7544aa5cfc
Pele <70554344+Pele12138@users.noreply.github.com>
2022-02-28 21:28:25 +0800

    function challenge #1 & arrays

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,4 +1,4 @@
-// 'use strcit';
+/* // 'use strcit';
 // function declaration
 function fruitProcessor(apples, oranges) {
     console.log(apples, oranges);
@@ -11,4 +11,39 @@ console.log(fruitProcessor(1, 2));
 //function expression
 const calAge2 = function (birthyear) {
     return 2030 - birthyear;
+} */
+
+// Challenge #1
+/*const calcAverage = (socre1, socre2, score3) => (socre1 + socre2 + score3) / 3;
+const avrDolphins = calcAverage(85, 54, 41);
+const avrKoalas = calcAverage(23, 34, 27);
+console.log(avrDolphins);
+console.log(avrKoalas);
+
+const checkWinner = function (avr1, avr2) {
+    if (avr1 >= avr2 * 2) {
+        console.log(`Koalas win (${avr1} vs. ${avr2})`);
+    } else if (avr2 >= avr1 * 2) {
+        console.log(`Dophins win (${avr2} vs. ${avr1})`);
+    } else {
+        console.log("No team wins");
+    }
 }
+
+checkWinner(avrKoalas, avrDolphins);*/
+
+//==================================//
+
+//Arrays
+const friends = ['Pele', 'Michael', 'Peter'];
+console.log(friends);
+
+const years = new Array(1998, 1999, 2000, 2001);
+
+console.log(friends[0]);
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
+
+const firstName = 'Pele';
+const pele = [firstName, 'Pan', friends, 2022 - 2001];
+console.log(pele);
