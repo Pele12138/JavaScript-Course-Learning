e9977cad2ddec1119bb4ca20ea9387413c9e639d
Pele <70554344+Pele12138@users.noreply.github.com>
2022-03-28 23:27:28 +0800

    Modal Overlay click

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,33 +1,29 @@
 'use strict';
 const getClass = (className) => {
-    return document.querySelector(className);
-}
+  return document.querySelector(className);
+};
 const modal = getClass('.modal');
 const overlay = getClass('.overlay');
 const btn = getClass('.close-modal');
-const hidden = document.querySelectorAll('.hidden')
+const hidden = document.querySelectorAll('.hidden');
 console.log(hidden);
-// only select the first element
-// const btns = document.querySelector('.show-modal');
 const openModal = () => {
-    // modal.style.display = 'block';
-    // overlay.style.display = 'block';
-    // modal.classList.remove('hidden');
-    // overlay.classList.remove('hidden');
-    for (const iterator of hidden) {
-        iterator.display = 'block';
-    }
-}
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
 const closeModal = () => {
-    // modal.style.display = 'none';
-    // overlay.style.display = 'none';
-    for (const iterator of hidden) {
-        iterator.display = 'none';
-    }
-}
+  if (!modal.classList.contains('hidden')) {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+  }
+};
 const btns = document.querySelectorAll('.show-modal');
 for (const iterator of btns) iterator.addEventListener('click', openModal);
 
 btn.addEventListener('click', closeModal);
 
+document.addEventListener('keydown', (e) => {
+  if (e.code === 'Escape') closeModal();
+});
 
+overlay.addEventListener('click', closeModal);
